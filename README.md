5c722ea4a04eb1f2a76660e2b3afc6233b80b6e8
Nuno Mendes <nhmendes@gmail.com>
2021-04-02 16:26:50 +0100

    added code coverage

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ To run the API just execute this in terminal:
 ```bash
 uvicorn app_fast:app --reload
 ```
+
+## Run tests with code coverage
+
+First setup CWD:
+
+```bash
+CWD="$(pwd)"
+```
+
+```bash
+pytest -v --cov=$CWD --cov-report html
+````
--- src/application/applicationservices/usecases/implementations/test_getuser.py
+++ src/application/applicationservices/usecases/implementations/test_getuser.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch, Mock, MagicMock
+from unittest.mock import Mock
 
 from uuid import UUID
 from src.domain.domaincore.gatewayinterfaces.users_gateway import UsersGateway
@@ -11,6 +11,7 @@ from src.application.applicationservices.usecases.implementations.get_user impor
 class TestGetUserById(unittest.TestCase):
 
     def test_get_existing_user(self):
+        # Arrange
         user_id: UUID = UUID('eac9bea1-46c7-4434-8c78-3f7d5de1e8d2')
         expected = User(
             user_id,
@@ -21,9 +22,11 @@ class TestGetUserById(unittest.TestCase):
         mock_gateway = Mock(UsersGateway)
         mock_gateway.get = Mock(return_value=expected)
 
+        # Act
         get_user_usecase = GetUserById(mock_gateway)
         actual = get_user_usecase.execute(expected.user_id)
 
+        # Assert
         mock_gateway.get.assert_called_with(expected.user_id)
 
         self.assertIsNotNone(actual)
